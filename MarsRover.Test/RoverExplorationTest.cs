35b7bc5eba3749f06996a80b1eef9e868cc2db85
Anıl Yıldırım <anilyildirim12@gmail.com>
2021-09-22 14:06:08 +0300

    Added OutOfPlateau test for rover.

--- MarsRover.Test/RoverExplorationTest.cs
+++ MarsRover.Test/RoverExplorationTest.cs
@@ -186,5 +186,68 @@ namespace MarsRover.Test
             Assert.Equal(3, roverThree.Coordinate.Y);
             Assert.Equal(HeadingType.N, roverThree.Coordinate.Heading);
         }
+
+        [Fact]
+        public void OutOfPlateau()
+        {
+            var plateau = new PlateauDto
+            {
+                UpperRightX = 3,
+                UpperRightY = 3
+            };
+
+            var roverOne = new RoverDto
+            {
+                Id = 1,
+                Coordinate = new RoverCoordinateDto
+                {
+                    X = 1,
+                    Y = 0,
+                    Heading = HeadingType.E
+                },
+                MoveInstructions = new MoveInstructionType[]
+                {
+                    MoveInstructionType.M,
+                    MoveInstructionType.M,
+                    MoveInstructionType.M,
+                    MoveInstructionType.M,
+                    MoveInstructionType.M
+                },
+                Success = true
+            };
+            var roverTwo = new RoverDto
+            {
+                Id = 2,
+                Coordinate = new RoverCoordinateDto
+                {
+                    X = 0,
+                    Y = 3,
+                    Heading = HeadingType.N
+                },
+                MoveInstructions = new MoveInstructionType[]
+                {
+                    MoveInstructionType.R,
+                    MoveInstructionType.R,
+                    MoveInstructionType.M,
+                    MoveInstructionType.M,
+                    MoveInstructionType.M,
+                    MoveInstructionType.M,
+                    MoveInstructionType.M
+                },
+                Success = true
+            };
+
+            RoverExploration.Discover(plateau, new List<RoverDto>() { roverOne, roverTwo });
+
+            // RoverOne
+            Assert.Equal(3, roverOne.Coordinate.X);
+            Assert.Equal(0, roverOne.Coordinate.Y);
+            Assert.Equal(HeadingType.E, roverOne.Coordinate.Heading);
+
+            // RoverTwo
+            Assert.Equal(0, roverTwo.Coordinate.X);
+            Assert.Equal(0, roverTwo.Coordinate.Y);
+            Assert.Equal(HeadingType.S, roverTwo.Coordinate.Heading);
+        }
     }
 }
